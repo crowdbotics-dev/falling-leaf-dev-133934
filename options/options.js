fe2f73ce6f4ea295738bc650d986aadff443afa5
Crowdbotics <engineering@crowdbotics.com>
2024-02-26 17:11:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "falling-leaf-dev-133934",
+  url: "https://falling-leaf-dev-133934.botics.co",
+  api: "https://falling-leaf-dev-133934.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
